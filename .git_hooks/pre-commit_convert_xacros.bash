fc34d25125af87d12c59e6fbc498231c650a7c32
Andrej Orsula <orsula.andrej@gmail.com>
2022-09-19 17:11:30 +0200

    Initial commit
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ .git_hooks/pre-commit_convert_xacros.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Pre-commit hook that convert xacros to URDF/SDF/SRDF. It makes sure that edits in xacros propagate to these files.
+
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+REPO_DIR="$(dirname "${SCRIPT_DIR}")"
+
+# Convert xacros to URDF, SDF and SRDF
+"${REPO_DIR}/phantomx_pincher_description/scripts/xacro2urdf.bash"
+"${REPO_DIR}/phantomx_pincher_description/scripts/xacro2sdf.bash"
+"${REPO_DIR}/phantomx_pincher_moveit_config/scripts/xacro2srdf.bash"
