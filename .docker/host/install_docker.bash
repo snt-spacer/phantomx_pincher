07e0cf3a86e7195ebab1ab6b484acf5d3f5bd03d
Andrej Orsula <orsula.andrej@gmail.com>
2023-10-06 22:22:38 +0200

    Improve UI of `install_docker.bash` script
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- .docker/host/install_docker.bash
+++ .docker/host/install_docker.bash
@@ -37,38 +37,66 @@ configure_nvidia_apt_repository() {
     sudo apt-get update
 }
 if check_nvidia_gpu; then
-    read -erp "Do you want to install support for NVIDIA GPU inside Docker containers? [Y/n]: " INSTALL_NVIDIA_SUPPORT
-    if ! command -v nvidia-container-toolkit >/dev/null 2>&1; then
-        if [[ "${INSTALL_NVIDIA_SUPPORT,,}" =~ (y|yes) && ! "${INSTALL_NVIDIA_SUPPORT,,}" =~ (n|no) ]]; then
-            DOCKER_VERSION="$(sudo docker version --format '{{.Server.Version}}' 2>/dev/null)"
-            MIN_VERSION_FOR_TOOLKIT="19.3"
-            if [ "$(printf '%s\n' "${MIN_VERSION_FOR_TOOLKIT}" "${DOCKER_VERSION}" | sort -V | head -n1)" = "$MIN_VERSION_FOR_TOOLKIT" ]; then
-                if ! command -v nvidia-container-toolkit >/dev/null 2>&1; then
-                    if command -v apt-get >/dev/null 2>&1; then
-                        configure_nvidia_apt_repository
-                        sudo apt-get install -y nvidia-container-toolkit
-                    elif command -v yum >/dev/null 2>&1; then
-                        curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo |
-                        sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-                        sudo yum install -y nvidia-container-toolkit
-                    else
-                        echo >&2 -e "\033[1;31mERROR: Supported package manager not found. Please install nvidia-container-toolkit manually.\033[0m"
-                    fi
+    echo -e "INFO: NVIDIA GPU detected."
+    DOCKER_VERSION="$(sudo docker version --format '{{.Server.Version}}' 2>/dev/null)"
+    MIN_VERSION_FOR_TOOLKIT="19.3"
+    if [ "$(printf '%s\n' "${MIN_VERSION_FOR_TOOLKIT}" "${DOCKER_VERSION}" | sort -V | head -n1)" = "$MIN_VERSION_FOR_TOOLKIT" ]; then
+        if ! command -v nvidia-container-toolkit >/dev/null 2>&1; then
+            while true; do
+                read -erp "Do you want to install NVIDIA Container Toolkit? [Y/n]: " INSTALL_NVIDIA_CONTAINER_TOOLKIT
+                case "${INSTALL_NVIDIA_CONTAINER_TOOLKIT,,}" in
+                    "" | y | yes)
+                        INSTALL_NVIDIA_CONTAINER_TOOLKIT=true
+                        break
+                        ;;
+                    n | no)
+                        INSTALL_NVIDIA_CONTAINER_TOOLKIT=false
+                        break
+                        ;;
+                esac
+            done
+            if [[ "${INSTALL_NVIDIA_CONTAINER_TOOLKIT}" = true ]]; then
+                if command -v apt-get >/dev/null 2>&1; then
+                    configure_nvidia_apt_repository
+                    sudo apt-get install -y nvidia-container-toolkit
+                elif command -v yum >/dev/null 2>&1; then
+                    curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo |
+                    sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+                    sudo yum install -y nvidia-container-toolkit
                 else
-                    echo "NVIDIA Container Toolkit is already installed."
+                    echo >&2 -e "\033[1;31mERROR: Supported package manager not found. Please install nvidia-container-toolkit manually.\033[0m"
                 fi
-            else
-                if ! command -v nvidia-docker >/dev/null 2>&1; then
-                    if command -v apt-get >/dev/null 2>&1; then
-                        configure_nvidia_apt_repository
-                        sudo apt-get install -y nvidia-docker2
-                        echo >&2 -e "\033[1;31mERROR: Supported package manager not found. Please install nvidia-docker2 manually.\033[0m"
-                    fi
-                else
-                    echo "NVIDIA Docker is already installed."
-                fi
+                sudo systemctl restart --now docker
             fi
-            sudo systemctl restart --now docker
+        else
+            echo -e "INFO: NVIDIA Container Toolkit is already installed."
+        fi
+    else
+        if ! command -v nvidia-docker >/dev/null 2>&1; then
+            while true; do
+                read -erp "Do you want to install NVIDIA Docker [Y/n]: " INSTALL_NVIDIA_DOCKER
+                case "${INSTALL_NVIDIA_DOCKER,,}" in
+                    "" | y | yes)
+                        INSTALL_NVIDIA_DOCKER=true
+                        break
+                        ;;
+                    n | no)
+                        INSTALL_NVIDIA_DOCKER=false
+                        break
+                        ;;
+                esac
+            done
+            if [[ "${INSTALL_NVIDIA_DOCKER}" = true ]]; then
+                if command -v apt-get >/dev/null 2>&1; then
+                    configure_nvidia_apt_repository
+                    sudo apt-get install -y nvidia-docker2
+                else
+                    echo >&2 -e "\033[1;31mERROR: Supported package manager not found. Please install nvidia-docker2 manually.\033[0m"
+                fi
+                sudo systemctl restart --now docker
+            fi
+        else
+            echo -e "INFO: NVIDIA Docker is already installed."
         fi
     fi
 fi
@@ -76,28 +104,24 @@ fi
 if [[ $(grep /etc/group -e "docker") != *"${USER}"* ]]; then
     [ -z "${PS1}" ]
     ## (Optional) Add user to docker group
-    read -erp "Do you want to add the current user ${USER} to the docker group? [Y/n]: " ADD_USER_TO_DOCKER_GROUP
-    if [[ "${ADD_USER_TO_DOCKER_GROUP,,}" =~ (y|yes) && ! "${ADD_USER_TO_DOCKER_GROUP,,}" =~ (n|no) ]]; then
+    while true; do
+        read -erp "Do you want to add the current user ${USER} to the docker group? [Y/n]: " ADD_USER_TO_DOCKER_GROUP
+        case "${ADD_USER_TO_DOCKER_GROUP,,}" in
+            "" | y | yes)
+                ADD_USER_TO_DOCKER_GROUP=true
+                break
+                ;;
+            n | no)
+                ADD_USER_TO_DOCKER_GROUP=false
+                break
+                ;;
+        esac
+    done
+    if [[ "${ADD_USER_TO_DOCKER_GROUP}" = true ]]; then
         sudo groupadd -f docker
         sudo usermod -aG docker "${USER}"
         echo -e "INFO: The current user ${USER} was added to the docker group. Please log out and log back in to apply the changes. Alternatively, run the following command to apply the changes in each new shell until you log out:\n\n\tnewgrp docker\n"
-    else
-        ## (Optional) Setup rootless mode if requested
-        read -erp "Do you want to run the Docker daemon as a non-root user (rootless mode)? [y/N]: " SETUP_ROOTLESS_MODE
-        if [[ "${SETUP_ROOTLESS_MODE,,}" =~ (y|yes) && ! "${SETUP_ROOTLESS_MODE,,}" =~ (n|no) ]]; then
-            # Install uidmap dependency if missing
-            if ! command -v newuidmap >/dev/null 2>&1; then
-                if command -v apt-get >/dev/null 2>&1; then
-                    sudo apt-get install -y uidmap
-                elif command -v dnf >/dev/null 2>&1; then
-                    sudo dnf install -y shadow-utils
-                elif command -v yum >/dev/null 2>&1; then
-                    sudo yum install -y shadow-utils
-                fi
-            fi
-            sudo systemctl disable --now docker
-            dockerd-rootless-setuptool.sh install --force
-            sudo systemctl restart --now docker
-        fi
     fi
+else
+    echo -e "INFO: The current user ${USER} is already in the docker group."
 fi
